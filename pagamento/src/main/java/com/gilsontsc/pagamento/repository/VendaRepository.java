bea33182250aa579e985f5bfd758134fce776b0e
GilsontTSC <gilson.costa@dcx.ufpb.br>
2021-01-13 18:13:38 -0300

    Criando os repositorios

---
+++ pagamento/src/main/java/com/gilsontsc/pagamento/repository/VendaRepository.java
@@ -0,0 +1,11 @@
+package com.gilsontsc.pagamento.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.gilsontsc.pagamento.entity.Venda;
+
+@Repository
+public interface VendaRepository extends JpaRepository<Venda, Long>{
+
+}
